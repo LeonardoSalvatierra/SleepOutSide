38db15a182696ba9687246e87fe1a99b291fd980
LeonardoSalvatierra <salvatierra.leonardo.sud@gmail.com>
2024-09-30 19:30:18 -0300

    error fixed

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,23 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
+// function addProductToCart(product) {
+//   // const currentStorage = getLocalStorage("so-cart");
+//   localStorage.clear();
+//   localStorage.setItem("so-cart", [product]);
+// }
+// add to cart button event handler
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
+  let products = JSON.parse(localStorage.getItem("so-cart"));
+  if (!products){
+    products = [];
+  }
+  products.push(product)
+  localStorage.setItem("so-cart", JSON.stringify(products));
+
+ }
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
