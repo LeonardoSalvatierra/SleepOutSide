7b88613b7df4a185372eae69827291a92134860d
LeonardoSalvatierra <salvatierra.leonardo.sud@gmail.com>
2024-10-06 02:32:44 -0300

    total price, and delete button

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,25 +4,43 @@ function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  // Mostrar el total y la div si hay productos
+  if (cartItems.length > 0) {
+    document.querySelector(".cart-footer").classList.remove("hide");
+    const total = calculateTotal(cartItems);
+    document.getElementById("cart-total").textContent = total.toFixed(2);
+  } else {
+    document.querySelector(".cart-footer").classList.add("hide");
+  }
 }
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
-
+    <a href="#" class="cart-card__image">
+      <img src="${item.Image}" alt="${item.Name}" />
+    </a>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+  </li>`;
   return newItem;
 }
 
+function calculateTotal(cartItems) {
+  return cartItems.reduce((total, item) => total + item.FinalPrice, 0);
+}
+
+function clearCart() {
+  localStorage.removeItem("so-cart"); // Elimina el carrito del localStorage
+  renderCartContents(); // Vuelve a renderizar el carrito
+}
+
+// Agregar el event listener al botón de eliminar todo
+document.getElementById("clear-cart").addEventListener("click", clearCart);
+
+// Renderizar el contenido del carrito al cargar la página
 renderCartContents();
