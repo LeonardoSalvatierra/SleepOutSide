7ddb3bbf28b874884294ed04e9089ecbf1210299
LeonardoSalvatierra <salvatierra.leonardo.sud@gmail.com>
2024-10-20 00:20:55 -0300

    changes

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -31,9 +31,7 @@ export default class ProductDetails {
     this.renderProductDetails("main");
     // once the HTML is rendered we can add a listener to Add to Cart button
     // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
-    document
-      .getElementById("addToCart")
-      .addEventListener("click", this.addToCart.bind(this));
+    document.getElementById("addToCart").addEventListener("click", this.addToCart.bind(this));
   }
   addToCart() {
     let cartContents = getLocalStorage("so-cart");
@@ -47,9 +45,6 @@ export default class ProductDetails {
   }
   renderProductDetails(selector) {
     const element = document.querySelector(selector);
-    element.insertAdjacentHTML(
-      "afterBegin",
-      productDetailsTemplate(this.product)
-    );
+    element.insertAdjacentHTML("afterBegin",productDetailsTemplate(this.product));
   }
 }
